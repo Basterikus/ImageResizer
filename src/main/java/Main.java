b7471279f79f0e91e1c2f36924d14c1256fbd5e7
Basterikus <102787942+Basterikus@users.noreply.github.com>
2022-11-16 17:10:30 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,19 +7,19 @@ import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
-    private static final int newWidth = 300;
-    private static final int processorCoreCount = Runtime.getRuntime().availableProcessors();
-    private static final Queue<File> filesQueue = new ConcurrentLinkedQueue<>();
-    private static final long start = System.currentTimeMillis();
+    private static final int NEW_WIDTH = 300;
+    private static final int PROCESSOR_CORE_COUNT = Runtime.getRuntime().availableProcessors();
+    private static final Queue<File> FILES_QUEUE = new ConcurrentLinkedQueue<>();
+    private static final long START = System.currentTimeMillis();
     private static final String SRC_FOLDER = "C:/src";
     private static final String DST_FOLDER = "C:/dst";
 
     public static void main(String[] args) {
         File srcDir = new File(SRC_FOLDER);
-        filesQueue.addAll(Arrays.asList(Objects.requireNonNull(srcDir.listFiles())));
+        FILES_QUEUE.addAll(Arrays.asList(Objects.requireNonNull(srcDir.listFiles())));
 
-        for (int i = 0; i < processorCoreCount; i++) {
-            new Thread(new ImageResizer(filesQueue, newWidth, DST_FOLDER, start)).start();
+        for (int i = 0; i < PROCESSOR_CORE_COUNT; i++) {
+            new Thread(new ImageResizer(FILES_QUEUE, NEW_WIDTH, DST_FOLDER, START)).start();
         }
     }
 }
