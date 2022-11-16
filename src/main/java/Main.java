c953e6452e1239aaf0e444f41db6b27550fcba59
Basterikus <102787942+Basterikus@users.noreply.github.com>
2022-11-16 17:05:12 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,16 +11,15 @@ public class Main {
     private static final int processorCoreCount = Runtime.getRuntime().availableProcessors();
     private static final Queue<File> filesQueue = new ConcurrentLinkedQueue<>();
     private static final long start = System.currentTimeMillis();
+    private static final String SRC_FOLDER = "C:/src";
+    private static final String DST_FOLDER = "C:/dst";
 
     public static void main(String[] args) {
-        String srcFolder = "C:/Users/andre/Desktop/src";
-        String dstFolder = "C:/Users/andre/Desktop/dst";
-
-        File srcDir = new File(srcFolder);
+        File srcDir = new File(SRC_FOLDER);
         filesQueue.addAll(Arrays.asList(Objects.requireNonNull(srcDir.listFiles())));
 
         for (int i = 0; i < processorCoreCount; i++) {
-            new Thread(new ImageResizer(filesQueue, newWidth, dstFolder, start)).start();
+            new Thread(new ImageResizer(filesQueue, newWidth, DST_FOLDER, start)).start();
         }
     }
 }
